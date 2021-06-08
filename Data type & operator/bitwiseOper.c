f6328c387c51a0d9f6003bf85e1160464a2ad4ad
unknown <kiruba.nandhu@gmail.com>
2021-06-08 14:45:32 +0530

    'bitwiseOperator'

--- Data type & operator/bitwiseOper.c
+++ Data type & operator/bitwiseOper.c
@@ -3,5 +3,19 @@ void main()
 {
 	// &, |, ^, >>, << --> perform bitwise operation
 	// convert dec to binary, and perform &, |, ^, >>, <<
-
+	int a = 4, b=8,c;
+	c = a&b;
+	printf("%d\n",c);
+	
+	c = a|b;
+	printf("%d\n",c);
+	
+	c = a^b;
+	printf("%d\n",c);
+	
+	c = b>>1;
+	printf("%d\n",c);
+	
+	c = b<<1;
+	printf("%d\n",c);
 }
